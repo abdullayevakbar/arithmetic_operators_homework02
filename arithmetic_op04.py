d2d37dce8efa6c8f3a94d076339bb3c138c4b41a
NewProject <uzbzarif@gmail.com>
2021-11-07 13:33:36 +0900

    op04

--- arithmetic_op04.py
+++ arithmetic_op04.py
@@ -1,11 +1,16 @@
 #Create a variable called 'number' and assign it the three-digit number.
-
+number = 351
 #Find the 'number' first digit and assign to x1.
-
+x1 = number%10
+number//=10
 #Find the 'number' second digit and assign to x2.
 
+x2 = number%10
+number//=10
 #Find the 'number' third digit and assign to x3.
 
+x3 = number%10
+number//=10
 #Create a variable called 'answer' and assign it the sum of the three digits.
 
 #print the sum of the three digits.
